86ccd3fb7a77d34c62a767dca23b7a14ed6fe0cc
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-03-25 08:46:43 -0500

    added sound and functions in consolelog only

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+
+
+
+//   // Gets Link for Theme Song
+
+//   var audioElement = document.createElement("audio");
+//   audioElement.setAttribute("src", "/music/bensound-theduel.mp3");
+
+//   // Theme Button
+//   $("#btn-on").on("click", function () {
+//     audioElement.play();
+//   });
+//   $("#btn-off").on("click", function () {
+//     audioElement.pause();
+//   });
+// });
