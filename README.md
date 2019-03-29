b560c1580875d2bd2d76294704f9d1a07128e21b
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-03-29 18:47:15 -0500

    complete

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unit-4-game
-CrystalsCollector Game
+
+# Coin Collector Game
+
+In this game, a random number will be guessed by the computer.
+
+There are four ancient coins below with a random value.
+
+By clicking on a coin, you will add its value to your total score
+
+Win the game by matching the random number and untold glory shall beseach you
+
+If your total score goes above the random number, the crowd will get angry
+
+- Theme music can be turned off and on as an option
+
+- if the user wins or loses there's an alert and boos can be heard except for promise in DOMexception.
+- Also, an image of Commodus with a thumbs up or down signifies wins and losses
+
+The project is linked to [github](https://dallasappraiser.github.io/Bootstrap-Portfolio/unit-4-game)
+and [github](https://dallasappraiser.github.io/unit-4-game)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,19 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   // declare arrays
 
   var wins = 0,
     losses = 0,
     totalScore = 0;
-  var audio = new Audio('assets//music/money.mp3'),
-    boo = new Audio('./assets//music/heckles.mp3'),
-    lion = new Audio('assets//music/lion.mp3'),
-    yay = new Audio('assets//music/applause.mp3');
+  var audio = new Audio("assets//music/money.mp3"),
+    boo = new Audio("./assets//music/heckles.mp3"),
+    lion = new Audio("assets//music/lion.mp3"),
+    yay = new Audio("assets//music/applause.mp3");
 
-  var images = ["assets/images/undecided.jpg", "assets/images/thumbs-up.jpg", "assets/images/thumbs-down.jpg"];
-
-
-  function resetAll() {
-
-
-    totalScore = 0;
-    randomNumber = parseInt(getRandomNumber(19, 120));
-    coin1 = parseInt(getRandomNumber(1, 12));
-    coin2 = parseInt(getRandomNumber(1, 12));
-    coin3 = parseInt(getRandomNumber(1, 12));
-    coin4 = parseInt(getRandomNumber(1, 12));
-    $(".random").text(randomNumber);
-    $(".score").text(totalScore);
-    boo.pause();
-    lion.pause();
-    yay.pause();
-  };
+  var images = [
+    "./assets/images/undecided.jpg",
+    "./assets/images/thumbs-up.jpg",
+    "./assets/images/thumbs-down.jpg"
+  ];
 
   // Generate random numbers
   var randomNumber = parseInt(getRandomNumber(19, 120)),
@@ -42,9 +28,27 @@ $(document).ready(function () {
     return Math.floor(Math.random() * (1 + a - b)) + b;
   }
 
+  // Clear all and generate new numbers
+
+  function resetAll() {
+    totalScore = 0;
+    randomNumber = parseInt(getRandomNumber(19, 120));
+    coinValues = {
+      coin1: parseInt(getRandomNumber(1, 12)),
+      coin2: parseInt(getRandomNumber(1, 12)),
+      coin3: parseInt(getRandomNumber(1, 12)),
+      coin4: parseInt(getRandomNumber(1, 12))
+    };
+
+    $(".random").text(randomNumber);
+    $(".score").text(totalScore);
+    boo.pause();
+    lion.pause();
+    yay.pause();
+  }
 
   // Add random value to coins
-  $(".coin-image").on("click", function () {
+  $(".coin-image").on("click", function() {
     var id = $(this).attr("id");
     totalScore += coinValues["coin" + id];
     audio.play();
@@ -55,45 +59,44 @@ $(document).ready(function () {
   // Track wins and losses
   function updateUser(randomNumber, totalScore) {
     if (randomNumber > totalScore) {
-
       $("#image-holder").html("<img src=" + images[0] + " width='440px'>");
     } else if (randomNumber < totalScore) {
-      losses++
-
-      $(".loser").text(losses);
+      losses++;
       boo.play();
+      $(".loser").text(losses);
       $("#image-holder").html("<img src=" + images[2] + " width='440px'>");
-      resetAll();
+      alert("You have made the Gods angry!");
 
+      resetAll();
     } else {
-      wins++
+      wins++;
       yay.play();
       $(".winner").text(wins);
       $("#image-holder").html("<img src=" + images[1] + " width='440px'>");
-      alert("Great Job, you shall be free with your score of " + totalScore)
-      // resetAll();
+      alert("Great Job, you shall not be fed to the lions!");
+      resetAll();
     }
   }
 
   //                      Music Buttons
 
   // Gets Link for Theme Song
-  var audioElement = document.createElement("audio")
+  var audioElement = document.createElement("audio");
   audioElement.setAttribute("src", "./assets/music/epic.mp3");
 
   // Theme Button
-  $(".btn-on").on("click", function () {
+  $(".btn-on").on("click", function() {
     audioElement.play();
-  })
-  $(".btn-off").on("click", function () {
+  });
+  $(".btn-off").on("click", function() {
     audioElement.pause();
   });
 
   // Play sound for Wins/Losses
 
   function preload() {
-    soundFormats('mp3', 'ogg');
-    mySound = loadSound('./assets/music/Applause.mp3');
+    soundFormats("mp3", "ogg");
+    mySound = loadSound("./assets/music/Applause.mp3");
   }
 
   function setup() {
